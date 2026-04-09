c4720f7d922ab57bae172b85ecc490f8a4c96be8
Neil <neil.earlam123@icloud.com>
2026-04-09 21:26:59 +0100

    Added base repo interfaces

---
+++ Backend/src/FileManager.Application/Common/Interfaces/IApplicationUserRepository.cs
@@ -0,0 +1,11 @@
+using FileManager.Domain.Common;
+using FileManager.Domain.Common.Errors;
+using FileManager.Domain.Models;
+
+namespace FileManager.Application.Common.Interfaces;
+
+public interface IApplicationUserRepository
+{
+    Task SaveAsync(ApplicationUser user, CancellationToken ct);
+    Task<Result<ApplicationUser, Error>> GetByAsync(string externalProviderId, CancellationToken ct);
+}
