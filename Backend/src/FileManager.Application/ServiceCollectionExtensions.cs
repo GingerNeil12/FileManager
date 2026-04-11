141fa648ce0c8505e2fa0cc92ba47cf7297fef0f
Neil <neil.earlam123@icloud.com>
2026-04-11 16:31:42 +0100

    Added Configuration support to Application layer

--- Backend/src/FileManager.Application/ServiceCollectionExtensions.cs
+++ Backend/src/FileManager.Application/ServiceCollectionExtensions.cs
@@ -2,13 +2,16 @@ using FileManager.Application.Workflows.CreateUser;
 
 using FluentValidation;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FileManager.Application;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddValidatorsFromAssembly(typeof(ServiceCollectionExtensions).Assembly);
         
